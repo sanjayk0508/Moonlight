6179123872e1dc26885bf2ef883f78783a5ec1e6
Sanjay <102804548+sanjayk0508@users.noreply.github.com>
2022-10-20 14:49:53 +0530

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -8,14 +8,14 @@ const Footer: FunctionComponent<FooterProps> = () => {
   return (
     <div className="bg-dark-lighten text-white flex justify-between items-center py-3 px-4 shadow-md mt-3">
       <p className="flex gap-2">
-        <span>Copyright _Fuocy</span>
-        <span className="hidden md:block"> &copy; 19/7/2022</span>
+        <span>Copyright _sanjay</span>
+        <span className="hidden md:block"> &copy; 6/6/2022</span>
       </p>
       <div className="flex gap-3 items-center">
         <p className="hidden md:block">Contact me: </p>
         <div className="flex gap-2">
           <a
-            href="https://github.com/fuocy"
+            href="https://github.com/sanjayk0508"
             target="_blank"
             rel="noreferrer"
             className="hover:text-[#6e5494] transition duration-300"
@@ -23,7 +23,7 @@ const Footer: FunctionComponent<FooterProps> = () => {
             <AiFillGithub size={25} />
           </a>
           <a
-            href="https://www.facebook.com/fuocy"
+            href="https://www.facebook.com"
             target="_blank"
             rel="noreferrer"
             className="hover:text-primary transition duration-300"
